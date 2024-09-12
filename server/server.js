175b586244459d058d246cdbbd454cdb9db5eecf
Success-akinyemi <brownsamuel915@gmail.com>
2024-09-11 23:28:15 -0700

    netlify.toml

--- server/server.js
+++ server/server.js
@@ -1,55 +1,81 @@
 import express from "express";
 import { config } from 'dotenv';
 config();
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
-// Import routes
-import authRoute from './routes/web/auth.routes.js';
-import userRoute from './routes/web/user.routes.js';
-import airtimeToCashRoute from './routes/web/airtimeToCash.routes.js';
-import FundingRoutes from './routes/web/funding.routes.js';
+import authRoute from './routes/web/auth.routes.js'
+import userRoute from './routes/web/user.routes.js'
+import airtimeToCashRoute from './routes/web/airtimeToCash.routes.js'
+import FundingRoutes from './routes/web/funding.routes.js'
 
-// Docs
+//DOCs
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 const swaggerJSDocs = YAML.load('./api.yaml');
 
-const app = express();
+const app = express()
+app.use('/api-doc',swaggerUI.serve,swaggerUI.setup(swaggerJSDocs))
+app.use(express.json())
+app.use(cookieParser())
+const allowedOrigins = [
+    process.env.CLIENT_URL,
+    process.env.NEBOUR_URL,
+    process.env.CLIENT_URL2,
+    process.env.CLIENT_URL3,
+    process.env.SERVER_URL,
+];
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    console.log('ORIGIN', origin)
 
-// Swagger documentation
-app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerJSDocs));
+    if (allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
-// Middlewares
-app.use(express.json());
-app.use(cookieParser());
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },    
+    credentials: true,
+};
 
-// CORS setup to allow requests from any origin
-app.use(cors({
-    origin: '*',  // Allow requests from any origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-}));
+app.use(cors(corsOptions));
 
-// Port
-const PORT = process.env.PORT || 9000;
 
-// Import DB connection
-import './connection/db.js';
+const PORT = process.env.PORT || 9000
 
-// HTTP GET request
+//Import DB
+import './connection/db.js'
+
+/**HTTP get request */
 app.get('/', (req, res) => {
-    res.status(201).json('Home GET Request');
-});
+    res.status(201).json('Home GET Request')
+})
 
-// Routes
-app.use('/api/web/auth', authRoute);
-app.use('/api/web/user', userRoute);
-app.use('/api/web/airtimeToCash', airtimeToCashRoute);
-app.use('/api/web/funding', FundingRoutes);
+/**ROUTES */
+//WEB
+app.use('/api/web/auth', authRoute)
+app.use('/api/web/user', userRoute)
+app.use('/api/web/airtimeToCash', airtimeToCashRoute)
+app.use('/api/web/funding', FundingRoutes)
 
-// Start server
+
+
+//INSTAGRAM
+
+
+
+
 app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
+    console.log(`Server runing on port http://localhost:${PORT}`)
+})
